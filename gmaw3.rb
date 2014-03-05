9f2a416eb51f93af67d3e066557266e48e34f780
JosuahManuel <josuah.manuel@hotmail.com>
2014-03-04 18:26:42 -0600

    gmaw3

---
+++ gmaw3.rb
@@ -0,0 +1,10 @@
+puts 'Speak on it to grandmama!'
+while command != 'BYE'
+    command = gets.chomp
+    if command != command.upcase
+        puts "HUH?! SPEAK UP, DEAR!"
+    else
+        year = "19"+rand(30..50).to_s
+        puts "NO, NOT SINCE #{year}!"
+    end
+end
